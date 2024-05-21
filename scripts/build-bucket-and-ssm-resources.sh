ead35dc3b4e6f7dae9fdc71bd93770866a0a5a33
hazworks <harrishutkin@gmail.com>
2024-05-22 08:45:55 +1000

    Update build-bucket-and-ssm-resources.sh

--- scripts/build-bucket-and-ssm-resources.sh
+++ scripts/build-bucket-and-ssm-resources.sh
@@ -6,6 +6,6 @@ aws cloudformation create-stack \
     --stack-name google-drive-bucket-and-ssm \
     --template-body file://bucket-and-ssm-resources.yaml \
     --capabilities CAPABILITY_AUTO_EXPAND \
-    --parameters ParameterKey=LambdaAndReportBucket,ParameterValue=your-bucket-name \
+    --parameters ParameterKey=LambdaAndReportBucket,ParameterValue=hazcloudbackup \
         ParameterKey=GoogleServiceAccountCredentials,ParameterValue=google-service-account-credentials \
         ParameterKey=GoogleDriveFolderId,ParameterValue=google-drive-folder-id
